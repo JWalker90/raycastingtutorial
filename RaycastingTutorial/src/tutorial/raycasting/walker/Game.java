c7051eac298dd62846e22963dc677cd7ef076999
Jonathan <Jonathan@Jonathan>
2021-02-20 03:46:36 -0500

    started tutorial

---
+++ RaycastingTutorial/src/tutorial/raycasting/walker/Game.java
@@ -0,0 +1,20 @@
+/* AUTHOR: Jonathan Walker
+ * COURSE: Extracurricular
+ * PURPOSE: Following a tutorial about raycasting graphics.
+ * STARTDATE: 02/19/21
+ */
+
+package tutorial.raycasting.walker;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+
+public class Game extends JFrame implements Runnable
+{
+	
+}//END Game class
---
+++ RaycastingTutorial/src/tutorial/raycasting/walker/MainClass.java
@@ -0,0 +1,17 @@
+/* AUTHOR: Jonathan Walker
+ * COURSE: Extracurricular
+ * PURPOSE: Following a tutorial about raycasting graphics.
+ * STARTDATE: 02/19/21
+ */
+
+package tutorial.raycasting.walker;
+
+public class MainClass
+{
+
+	public static void main(String[] args)
+	{
+		
+	}// END main method
+
+}// END main class
